54d95e3710b69f2bad73db5cc612e8f563083b5b
enesb58 <enesbasyurt10@gmail.com>
2025-08-30 02:31:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class ChangelogModal(Modal, title="Maak een Changelog"):
         embed.set_footer(text=f"Noorderveen Roleplay - Update by:  {interaction.user.name}")
         
         # Voeg een timestamp toe (huidige tijd)
-        embed.timestamp = datetime.now(timezone.utc)
+        embed.timestamp = datetime.strptime(datum_str, "%d-%m-%Y %H:%M").replace(tzinfo=timezone.utc)
 
         # Stuur de embed naar het kanaal waar het commando is gebruikt
         await interaction.channel.send(embed=embed)
